cba7a670e13946be52e1e3d609e4bb835983183e
Dan MacKinlay <myopenid@email.possumpalace.org>
2017-04-16 20:06:04 +1000

    try to fix integration

--- branching_process/cts/background.py
+++ branching_process/cts/background.py
@@ -20,7 +20,9 @@ class BackgroundKernel(InfluenceKernel):
             kappa=None,
             *args, **fixed_args):
         self._default_kwargs = fixed_args
-        self._default_kwargs.setdefault('kappa', np.ones(n_bases))
+        self._default_kwargs.setdefault(
+            'kappa',
+            kappa if kappa is not None else np.ones(n_bases))
         self.n_bases = n_bases
         # super(BackgroundKernel, self).__init__(*args)
 
@@ -55,12 +57,22 @@ class AdditiveStepKernel(BackgroundKernel):
     """
     def __init__(
             self,
-            t_end,
-            n_bases=100,
+            t_end=None,
+            n_bases=None,
             *args,
             **fixed_args
             ):
+        if t_end is None:
+            t_end = fixed_args.get('tau', [0, 100])[-1]
         self.t_end = t_end
+        if n_bases is None:
+            if fixed_args.get('tau', None) is not None:
+                n_bases = np.asarray(fixed_args.get('tau')).size - 1
+            elif fixed_args.get('kappa', None) is not None:
+                n_bases = np.asarray(fixed_args.get('kapp')).size
+            else:
+                n_bases = 100
+        self.n_bases = n_bases
         fixed_args.setdefault(
             'tau',
             np.linspace(0, t_end, n_bases+1, endpoint=True)
@@ -76,13 +88,14 @@ class AdditiveStepKernel(BackgroundKernel):
         kappa = self.get_param('kappa', **kwargs)
         mu = self.get_param('mu', 0.0, **kwargs)
         kappa = np.maximum(kappa, -mu)
-        t = np.reshape(t, (-1, 1))
-        each = (
-            (t > tau[:-1].reshape(1, -1)) -
-            (t > tau[1:].reshape(1, -1))
+        tt = np.reshape(t, (-1, 1))
+        stepwise_mask = (
+            (tt > tau[:-1].reshape(1, -1)) -
+            (tt > tau[1:].reshape(1, -1))
         )
+        from IPython.core.debugger import Tracer; Tracer()()
         return np.sum(
-            each * np.reshape(kappa, (1, -1)),
+            stepwise_mask * np.reshape(kappa, (1, -1)),
             1
         ) + mu
 
@@ -104,6 +117,27 @@ class AdditiveStepKernel(BackgroundKernel):
             each * np.reshape(kappa, (1, -1)),
             1
         ) + (mu * t.ravel())
+
+    def integrate2(self, t, *args, **kwargs):
+        """
+        This is tedious to construct since
+        it needs to be autograd differentiable, which splines are not.
+        So we do it in idiot python.
+        """
+        tau = self.get_param('tau', **kwargs)
+        kappa = self.get_param('kappa', **kwargs)
+        mu = self.get_param('mu', 0.0, **kwargs)
+        kappa = np.maximum(kappa, -mu)
+        tau_delta = np.diff(tau)
+        left_edge = np.zeros_like(tau)
+        left_edge[1:] = tau_delta * kappa
+        partial = np.zeros_like(t)
+        for i in range(tau.size-1):
+            partial = partial + (
+                (t >= tau[i]) * (t < tau[i+1]) * (t - tau[i])
+            ) * kappa[i] + left_edge[i]
+        t = np.reshape(t, (-1, 1))
+        return partial + (mu * t.ravel())
 
     def majorant(self, t, *args, **kwargs):
         kappa = self.get_param('kappa', **kwargs)
