acf38bba0a443384553217e1bb7e1bd5530e4d6f
Dan MacKinlay <myopenid@email.possumpalace.org>
2017-04-16 20:18:10 +1000

    multiplicateive kernel now minimal

--- branching_process/cts/background.py
+++ branching_process/cts/background.py
@@ -149,68 +149,13 @@ class AdditiveStepKernel(BackgroundKernel):
         return np.ones_like(t) * (mu + np.amax(kappa))
 
 
-class MultiplicativeStepKernel(BackgroundKernel):
+class MultiplicativeStepKernel(AdditiveStepKernel):
     """
     Piecewise-constant rate.
     This is presumably for background rate modelling.
     """
-    def __init__(
-            self,
-            t_end,
-            n_bases=100,
-            *args,
-            **fixed_args
-            ):
-        self.t_end = t_end
-        fixed_args.setdefault(
-            'tau',
-            np.linspace(0, t_end, n_bases+1, endpoint=True)
-        )
-        super(MultiplicativeStepKernel, self).__init__(
-            n_bases=n_bases,
-            *args, **fixed_args)
-
-    def __call__(self, t, *args, **kwargs):
-        """
-        """
-        tau = self.get_param('tau', **kwargs)
-        kappa = self.get_param('kappa', **kwargs)
-        mu = self.get_param('mu', 0.0, **kwargs)
-        kappa = np.maximum(kappa, -mu)
-        t = np.reshape(t, (-1, 1))
-        each = (
-            (t > tau[:-1].reshape(1, -1)) -
-            (t > tau[1:].reshape(1, -1))
-        )
-        return (np.sum(
-            each * np.reshape(kappa, (1, -1)),
-            1
-        ) + 1) * mu
-
-    def integrate(self, t, *args, **kwargs):
-        tau = self.get_param('tau', **kwargs)
-        kappa = self.get_param('kappa', **kwargs)
-        mu = self.get_param('mu', 0.0, **kwargs)
-        kappa = np.maximum(kappa, -mu)
-        t = np.reshape(t, (-1, 1))
-        delta = np.diff(tau)
-        each = np.maximum(
-            0, (t - tau[:-1].reshape(1, -1))
-        )
-        each = np.minimum(
-            each,
-            delta.reshape(1, -1)
-        )
-        return (np.sum(
-            each * np.reshape(kappa, (1, -1)),
-            1
-        ) + 1) * mu * t.ravel()
-
-    def majorant(self, t, *args, **kwargs):
-        kappa = self.get_param('kappa', **kwargs)
-        mu = self.get_param('mu', 0.0, **kwargs)
-        kappa = np.maximum(kappa, -mu)
-        return np.ones_like(t) * (mu * np.amax(kappa))
+    def omega(self, kappa, mu, **kwargs):
+        return (1 + np.maximum(kappa, -1)) * mu
 
 
 def as_background_kernel(
